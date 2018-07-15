5b8591ab2a738d80283f2ab2503c9a2d6fdf0e77
mechmqx <mechmqx@gmail.com>
2018-07-15 11:41:57 +0800

    my first gles demo under linux in glfw style

---
+++ glfw_style/build/CMakeFiles/testGLFW.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testGLFW.dir/src/simple.c.o"
+  "CMakeFiles/testGLFW.dir/deps/glad.c.o"
+  "testGLFW.pdb"
+  "testGLFW"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testGLFW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
